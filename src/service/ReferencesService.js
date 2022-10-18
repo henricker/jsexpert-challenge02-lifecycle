e8e4454130c29bf61b3535537e79fa4fcab7f532
henricker <henriquevieira@alu.ufc.br>
2022-10-18 09:40:36 -0300

    feat: finished reference exercises

--- src/service/ReferencesService.js
+++ src/service/ReferencesService.js
@@ -1,21 +1,23 @@
 class ReferencesService {
   getCounterCopyingReferences(object) {
     // TODO: assing object to a new variable and return it.
-    return null;
+    const newObject = object;
+    return newObject;
   }
 
   getCounterWithoutCopyingReferences(object) {
     // TODO: assing object to a new variable and return it.
-    return null;
+    const newObject = {...object};
+    return newObject;
   }
 
-  /*
+
   // @TIP: if you want to do the extra sub-challenge, uncomment and implement the following function:
   getCounterWithoutDeepCopyingReferences(object) {
     // TODO: assing object to a new variable and return it.
-    return null;
+    return JSON.parse(JSON.stringify(object));
   }
-  */
+
 }
 
 module.exports = ReferencesService;
